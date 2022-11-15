779078f563b36efda1add6b98c976d06f084798f
b-capra <caprabmc@gmail.com>
2022-11-14 22:52:17 -0500

    Clean up messy divs
    No longer need seperate divs given how the 'thanks' state is being
    generated in the js. This helps simplify and shorten the HTML and
    JS files. Changed the impacted CSS rules to match new structure

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const ratingState = document.querySelector('.rating-items');
-const thanksState = document.querySelector('.thank-items');
+const card = document.querySelector('.card');
 const ratings = document.querySelectorAll('.rating');
 const submit = document.querySelector('.submit');
 let selected = '';
@@ -25,29 +24,30 @@ function updateRating(newRating) {
 }
 
 function clearRating() {
-  while (ratingState.firstChild) {
-    ratingState.removeChild(ratingState.firstChild)
+  while (card.firstChild) {
+    card.removeChild(card.firstChild)
   }
 }
 
 function submitRating() {
+  card.classList.add('thanks')
   const tyImage = document.createElement('img')
   tyImage.setAttribute('src', './img/illustration-thank-you.svg')
   tyImage.classList.add('ty-image')
-  thanksState.appendChild(tyImage)
+  card.appendChild(tyImage)
   const selection = document.createElement('div')
   selection.classList.add('selection')
   const selText = document.createElement('p')
   selText.textContent = `You selected ${selected} out of 5`
   selText.classList.add('sel-text')
   selection.appendChild(selText)
-  thanksState.appendChild(selection)
+  card.appendChild(selection)
   const title = document.createElement('p')
   title.textContent = 'Thank you!'
   title.classList.add('title')
-  thanksState.appendChild(title)
+  card.appendChild(title)
   const para = document.createElement('p')
   para.textContent = "We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!"
   para.classList.add('center', 'para')
-  thanksState.appendChild(para)
+  card.appendChild(para)
 }
